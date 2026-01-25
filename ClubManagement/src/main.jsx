0a66c018cfac7c743c23e5a9b2f88eeb35d50242
andresnserna <andresnserna@icloud.com>
2026-01-24 21:07:20 -0600

    reorganized folder structure and added login and inventory pages (then added NotFoundPage for undefined routes)

--- ClubManagement/src/main.jsx
+++ ClubManagement/src/main.jsx
@@ -1,14 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Inventory from './pages/Inventory.jsx';
 import App from './App.jsx'
-import { BrowserRouter } from "react-router-dom";
+import NotFoundPage from './pages/NotFoundPage.jsx';
 
+const router = createBrowserRouter([
+  {path: "/", element: <App />},
+  {path: "/inventory", element: <Inventory/>},
+  {path: "*", element: <NotFoundPage />},
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <RouterProvider router={router} />
   </StrictMode>,
 )
