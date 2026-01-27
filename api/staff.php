ca0d1192d5bcce1edb3284263ae61362fd94c590
Javier A Zavala <javier.zavala2021@gmail.com>
2026-01-27 15:22:09 -0600

    Replace db.php inclusion with api.php

--- api/staff.php
+++ api/staff.php
@@ -3,7 +3,7 @@
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 
-include_once 'db.php';
+include_once 'api.php';
 
 $query = "SELECT id, name, role, hourly_rate FROM staff";
 
