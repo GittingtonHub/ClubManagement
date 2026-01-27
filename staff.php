64dcf7e6af3aa27db42c363abfa6ccc4dee6a8b6
Javier A Zavala <javier.zavala2021@gmail.com>
2026-01-27 13:49:12 -0600

    Add staff.php to fetch and return staff data

---
+++ staff.php
@@ -0,0 +1,17 @@
+<?php
+// where to fetch the results http://167.99.165.60/api/staff.php
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+
+include_once 'db.php';
+
+$query = "SELECT id, name, role, hourly_rate FROM staff";
+
+$stmt = $conn->prepare($query);
+$stmt->execute();
+
+$data = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+echo json_encode($data);
+?>
+
