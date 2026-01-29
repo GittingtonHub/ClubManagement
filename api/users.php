6b74e46df1ef76dae85eed9a09625759abe04acd
Javier A Zavala <javier.zavala2021@gmail.com>
2026-01-29 13:00:22 -0600

    Enhance comments for user data security in users.php
    Added comments to clarify security measures for user data handling.

--- api/users.php
+++ api/users.php
@@ -1,5 +1,9 @@
 <?php
 // where to fetch the results http://167.99.165.60/api/users.php
+// DO NOT ALLOW USERS OR FRONTEND TO SEE THIS AS IT WOULD BE COMPROMISING TO OUR USERS, WE SHOULD ONLY SEE IF THE USER PASOWRD
+// AND USERNAME ARE IN THE DB OR THEY ARE THE SAME
+// will prob change to see if name in username  else error
+// check if password matches password FOR SPECIFIC USERNAME
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 
