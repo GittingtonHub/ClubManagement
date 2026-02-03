b298208742b8e0a657aaf1ca0bc6cb43f256917e
andresnserna <andresnserna@icloud.com>
2026-02-02 19:30:35 -0600

    installed composer, and added config file for database credentials.

--- api/api.php
+++ api/api.php
@@ -2,10 +2,13 @@
 
 // CALL THIS FILE TO CONNECT TO THE DATABASE. WHEN POST REMEBER TO SANITIZE FIRST
 // SHOULD NOT BE SHOWN IN FRONTEND, IT WOULD BE A PROBLEM TO THE EVERYTHINGGGG
-$host = "localhost";
-$db_name = "ClubManagementDB";
-$username = "Javi";
-$password = "SpideyArc#2408";
+require_once __DIR__ . '/config.php';
+
+$host = DB_HOST;
+$db_name = DB_NAME;
+$username = DB_USER;
+$password = DB_PASSWORD;
+
 
 try {
     $conn = new PDO("mysql:host=$host;dbname=$db_name", $username, $password);
